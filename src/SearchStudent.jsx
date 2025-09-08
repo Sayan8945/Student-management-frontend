2ef1bca295ea1860424d26d4a625a2b55f602ed1
Sayan8945 <sayanpub2020@gmail.com>
2025-09-09 01:53:36 +0530

    linkRoute

--- src/SearchStudent.jsx
+++ src/SearchStudent.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 function SearchStudent() {
   const [formData, setFormData] = useState({
